2dd9ea525ff53ed0875bf151a80feb0cb21c7018
Eisuke Ishii(Johnny) <johnny@unitedstar-tech.com>
2021-04-22 10:17:52 +0900

    Mail2PostgreSQL

---
+++ config.py
@@ -0,0 +1,9 @@
+address = "***E=MAIL***"
+password = "***POP3 PASSWORD***"
+pop3_address = "pop.gmail.com"
+pop3_port = 995
+db_host = "***DB ADDRESS***"
+db_port = "5432"
+db_user = "***DB USER***"
+db_password = "***DB PASSWORD***"
+db_name = "***DB NAME***"
