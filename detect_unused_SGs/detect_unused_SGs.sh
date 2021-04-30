772e5933f6aa8cbb305d29c0c69e8a2bea45049a
Johnny E.Ishey <johnny@unitedstar-tech.com>
2021-05-01 00:23:41 +0900

    Fix

--- detect_unused_SGs/detect_unused_SGs.sh
+++ detect_unused_SGs/detect_unused_SGs.sh
@@ -5,35 +5,35 @@ export AWS_DEFAULT_REGION=ap-northeast-1
 SG=(`aws ec2 describe-security-groups --group-ids | grep SECURITYGROUPS | awk -F"\t" '{print $3}' | sort -u`)
 for CHECK in ${SG[*]}
 do
-    aws ec2 describe-instances | grep  $CHECK >/dev/null
-    if [ $? -eq 0 ]
-    then
-        continue
-    fi
-    aws elb describe-load-balancers | grep  $CHECK >/dev/null
-    if [ $? -eq 0 ]
-    then
-        continue
-    fi
-    aws elbv2 describe-load-balancers | grep  $CHECK >/dev/null
-    if [ $? -eq 0 ]
-    then
-        continue
-    fi
-    aws rds describe-db-instances | grep  $CHECK >/dev/null
-    if [ $? -eq 0 ]
-    then
-        continue
-    fi
-    aws rds describe-db-clusters | grep  $CHECK >/dev/null
-    if [ $? -eq 0 ]
-    then
-        continue
-    fi
-    aws elasticache describe-cache-clusters | grep  $CHECK >/dev/null
-    if [ $? -eq 0 ]
-    then
-        continue
-    fi
-    echo $CHECK
+	aws ec2 describe-instances | grep  $CHECK >/dev/null
+	if [ $? -eq 0 ]
+	then
+		continue
+	fi
+	aws elb describe-load-balancers | grep  $CHECK >/dev/null
+	if [ $? -eq 0 ]
+	then
+		continue
+	fi
+	aws elbv2 describe-load-balancers | grep  $CHECK >/dev/null
+	if [ $? -eq 0 ]
+	then
+		continue
+	fi
+	aws rds describe-db-instances | grep  $CHECK >/dev/null
+	if [ $? -eq 0 ]
+	then
+		continue
+	fi
+	aws rds describe-db-clusters | grep  $CHECK >/dev/null
+	if [ $? -eq 0 ]
+	then
+		continue
+	fi
+	aws elasticache describe-cache-clusters | grep  $CHECK >/dev/null
+	if [ $? -eq 0 ]
+	then
+		continue
+	fi
+	echo $CHECK
 done
