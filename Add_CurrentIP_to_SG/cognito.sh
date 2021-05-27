4eda172c362ef2857220ed0cbea5e7a3492c7ced
Johnny E.Ishey <johnny@unitedstar-tech.com>
2021-05-27 13:39:26 +0900

    Fix

--- Add_CurrentIP_to_SG/cognito.sh
+++ Add_CurrentIP_to_SG/cognito.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export LANG=C
+export AWS_DEFAULT_OUTPUT=text
 ID=$1
 Pass=$2
-UserPoolId=ap-northeast-1_OzULknGCT
-ClientId=23phd55fe9h61f21bgk7qlik7c
+UserPoolId=
+ClientId=
 API_Gateway=https://1li6epuzq0.execute-api.ap-northeast-1.amazonaws.com/test
 token=`aws cognito-idp admin-initiate-auth --user-pool-id $UserPoolId --client-id $ClientId --auth-flow ADMIN_NO_SRP_AUTH --auth-parameters USERNAME=$ID,PASSWORD=$Pass --query "AuthenticationResult.IdToken"`
 curl -H "Authorization:$token" $API_Gateway
--- Add_CurrentIP_to_SG/cognito_init.sh
+++ Add_CurrentIP_to_SG/cognito_init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export LANG=C
+export AWS_DEFAULT_OUTPUT=text
 ID=$1
 Pass=$2
-UserPoolId=ap-northeast-1_OzULknGCT
-ClientId=23phd55fe9h61f21bgk7qlik7c
+UserPoolId=
+ClientId=
 temp_pass=password
 session=`aws cognito-idp admin-initiate-auth --user-pool-id $UserPoolId --client-id $ClientId --auth-flow ADMIN_NO_SRP_AUTH --auth-parameters USERNAME=$ID,PASSWORD=$temp_pass | head -1 | awk '{print $2}'`
 aws cognito-idp admin-respond-to-auth-challenge --user-pool-id $UserPoolId --client-id $ClientId --challenge-name NEW_PASSWORD_REQUIRED --challenge-responses NEW_PASSWORD=$Pass,USERNAME=$ID --session "$session"
