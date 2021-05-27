b71215c4be83703343df1a91d0266f71241e6832
Johnny E.Ishey <johnny@unitedstar-tech.com>
2021-05-27 15:00:34 +0900

    Fix

--- Add_CurrentIP_to_SG/cognito.sh
+++ Add_CurrentIP_to_SG/cognito.sh
@@ -5,6 +5,6 @@ ID=$1
 Pass=$2
 UserPoolId=
 ClientId=
-API_Gateway=https://1li6epuzq0.execute-api.ap-northeast-1.amazonaws.com/test
+API_Gateway=
 token=`aws cognito-idp admin-initiate-auth --user-pool-id $UserPoolId --client-id $ClientId --auth-flow ADMIN_NO_SRP_AUTH --auth-parameters USERNAME=$ID,PASSWORD=$Pass --query "AuthenticationResult.IdToken"`
 curl -H "Authorization:$token" $API_Gateway
