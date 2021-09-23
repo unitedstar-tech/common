e1d71362cd9f1e810ef065dc1072f21bdebdc2af
Johnny <eisuke.ishii@2nd-community.com>
2021-09-23 18:46:31 +0900

    Multi-region support, multi-processing support

--- Add_CurrentIP_to_SG/Add_CurrentIP_to_SG.py
+++ Add_CurrentIP_to_SG/Add_CurrentIP_to_SG.py
@@ -1,6 +1,7 @@
 import boto3
 import datetime
 import os
+from multiprocessing import Process
 def lambda_handler(event, context):
 	try:
 		sip = event['sip'] + "/32"
@@ -8,6 +9,9 @@ def lambda_handler(event, context):
 	except:
 		sip = "1.2.3.4/32"
 	ec2 = boto3.client('ec2')
+	regions = list()
+	for i in ec2.describe_regions()['Regions']:
+		regions.append(i['RegionName'])
 	try:
 		sg_name = os.environ['sg_name']
 	except:
@@ -18,27 +22,37 @@ def lambda_handler(event, context):
 		duration = 3
 	now = datetime.datetime.now()
 	invalid = now - datetime.timedelta(days=duration)
+	for region in regions:
+		args = (region, sip, sg_name, now, invalid)
+		Process(target=each_region, args=args).start()
+	return sip
+
+def each_region(region, sip, sg_name, now, invalid):
+	ec2 = boto3.client('ec2', region_name=region)
 	vpcs = list()
 	for vpc in ec2.describe_vpcs()['Vpcs']:
 		vpcs.append(vpc['VpcId'])
 	for vpc in vpcs:
-		vpc_sg_name = vpc + '-' + sg_name
-		sgs = ec2.describe_security_groups(Filters=[{'Name': 'group-name', 'Values': [vpc_sg_name]}, {'Name': 'vpc-id', 'Values': [vpc]}])['SecurityGroups']
+		args = (ec2, region, sip, vpc, sg_name, now, invalid)
+		Process(target=each_vpc, args=args).start()
+
+def each_vpc(ec2, region, sip, vpc, sg_name, now, invalid):
+	vpc_sg_name = vpc + '-' + sg_name
+	sgs = ec2.describe_security_groups(Filters=[{'Name': 'group-name', 'Values': [vpc_sg_name]}, {'Name': 'vpc-id', 'Values': [vpc]}])['SecurityGroups']
+	if sgs:
+		sg_id = sgs[0]['GroupId']
+		sgs = sgs[0].get('IpPermissions')
 		if sgs:
-			sg_id = sgs[0]['GroupId']
-			sgs = sgs[0].get('IpPermissions')
-			if sgs:
-				sgs = sgs[0].get('IpRanges')
-				for sg in sgs:
-					if sg.get('Description') and (datetime.datetime.strptime(sg.get('Description'), '%Y-%m-%d %H:%M:%S.%f') <= invalid or sip in sg['CidrIp']):
-						ec2.revoke_security_group_ingress(GroupId=sg_id, IpPermissions=[{'FromPort': -1, 'IpProtocol': '-1', 'ToPort': -1, 'IpRanges': [sg]}])
-						print("Removed " + str(sg) + " from " + str(sg_id))
-		else:
-			sg_id = ec2.create_security_group(Description=vpc_sg_name, GroupName=vpc_sg_name, VpcId=vpc)['GroupId']
-			print("Created " + str(sg_id))
-		try:
-			ec2.authorize_security_group_ingress(GroupId=sg_id, IpPermissions=[{'FromPort': -1, 'IpProtocol': '-1', 'ToPort': -1, 'IpRanges': [{'CidrIp': sip, 'Description': str(now)}]}])
-			print("Add " + sip + " to " + str(sg_id) + " at " + str(now))
-		except:
-			pass
-	return sip
+			sgs = sgs[0].get('IpRanges')
+			for sg in sgs:
+				if sg.get('Description') and (datetime.datetime.strptime(sg.get('Description'), '%Y-%m-%d %H:%M:%S.%f') <= invalid or sip in sg['CidrIp']):
+					ec2.revoke_security_group_ingress(GroupId=sg_id, IpPermissions=[{'FromPort': -1, 'IpProtocol': '-1', 'ToPort': -1, 'IpRanges': [sg]}])
+					print("Removed " + str(sg) + " from " + str(sg_id) + ", " + str(vpc) + ", " + str(region))
+	else:
+		sg_id = ec2.create_security_group(Description=vpc_sg_name, GroupName=vpc_sg_name, VpcId=vpc)['GroupId']
+		print("Created " + str(sg_id))
+	try:
+		ec2.authorize_security_group_ingress(GroupId=sg_id, IpPermissions=[{'FromPort': -1, 'IpProtocol': '-1', 'ToPort': -1, 'IpRanges': [{'CidrIp': sip, 'Description': str(now)}]}])
+		print("Add " + sip + " to " + str(sg_id) + ", " + str(vpc) + ", " + str(region) + " at " + str(now))
+	except:
+		print("Error!\t" + vpc + "\t" + sg_name)
--- Add_CurrentIP_to_SG/regular_rotation.py
+++ Add_CurrentIP_to_SG/regular_rotation.py
@@ -1,8 +1,13 @@
 import boto3
 import datetime
 import os
+from multiprocessing import Process
 def lambda_handler(event, context):
+	# 変数定義
 	ec2 = boto3.client('ec2')
+	regions = list()
+	for i in ec2.describe_regions()['Regions']:
+		regions.append(i['RegionName'])
 	try:
 		sg_name = os.environ['sg_name']
 	except:
@@ -13,19 +18,31 @@ def lambda_handler(event, context):
 		duration = 3
 	now = datetime.datetime.now()
 	invalid = now - datetime.timedelta(days=duration)
+
+	# 各リージョン毎のSubprocessを起動
+	for region in regions:
+		args = (region, sg_name, invalid)
+		Process(target=each_region, args=args).start()
+	return "OK"
+
+def each_region(region, sg_name, invalid):
+	ec2 = boto3.client('ec2', region_name=region)
 	vpcs = list()
 	for vpc in ec2.describe_vpcs()['Vpcs']:
 		vpcs.append(vpc['VpcId'])
 	for vpc in vpcs:
-		vpc_sg_name = vpc + '-' + sg_name
-		sgs = ec2.describe_security_groups(Filters=[{'Name': 'group-name', 'Values': [vpc_sg_name]}, {'Name': 'vpc-id', 'Values': [vpc]}])['SecurityGroups']
+		args = (ec2, vpc, sg_name, invalid)
+		Process(target=each_vpc, args=args).start()
+
+def each_vpc(ec2, vpc, sg_name, invalid):
+	vpc_sg_name = vpc + '-' + sg_name
+	sgs = ec2.describe_security_groups(Filters=[{'Name': 'group-name', 'Values': [vpc_sg_name]}, {'Name': 'vpc-id', 'Values': [vpc]}])['SecurityGroups']
+	if sgs:
+		sg_id = sgs[0]['GroupId']
+		sgs = sgs[0].get('IpPermissions')
 		if sgs:
-			sg_id = sgs[0]['GroupId']
-			sgs = sgs[0].get('IpPermissions')
-			if sgs:
-				sgs = sgs[0].get('IpRanges')
-				for sg in sgs:
-					if sg.get('Description') and datetime.datetime.strptime(sg.get('Description'), '%Y-%m-%d %H:%M:%S.%f') <= invalid:
-						ec2.revoke_security_group_ingress(GroupId=sg_id, IpPermissions=[{'FromPort': -1, 'IpProtocol': '-1', 'ToPort': -1, 'IpRanges': [sg]}])
-						print("Removed " + str(sg) + " from " + str(sg_id))
-	return "OK"
+			sgs = sgs[0].get('IpRanges')
+			for sg in sgs:
+				if sg.get('Description') and datetime.datetime.strptime(sg.get('Description'), '%Y-%m-%d %H:%M:%S.%f') <= invalid:
+					ec2.revoke_security_group_ingress(GroupId=sg_id, IpPermissions=[{'FromPort': -1, 'IpProtocol': '-1', 'ToPort': -1, 'IpRanges': [sg]}])
+					print("Removed " + str(sg) + " from " + str(sg_id))
