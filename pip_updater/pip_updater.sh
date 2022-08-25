a3800d86ad052fcaf5078d2bb4eaa07b8310e877
Eisuke Ishii <eisuke.ishii@2nd-community.com>
2022-08-25 18:07:39 +0900

    Add pip_updater

---
+++ pip_updater/pip_updater.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export LANG=C
+if [ -n "$1" ]
+then
+	python=$1
+else
+	python=`which python3`
+fi
+$python -m pip install -U pip
+num=`$python -m pip list -o | wc -l`
+num=`expr $num - 2`
+packages=(`$python -m pip list -o | tail -$num | awk '{print $1}'`)
+for i in ${packages[@]}
+do
+	$python -m pip install -U $i || continue
+done
